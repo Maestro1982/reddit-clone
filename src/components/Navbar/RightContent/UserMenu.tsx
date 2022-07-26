6b59192087c40e87b40e1e02cd4361a24506cc4e
Maestro1982 <kris.tote@telenet.be>
2022-07-26 13:40:06 +0200

    second part complete firebase auth icons in navbar and multiple modal components

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -1,18 +1,25 @@
 import {
+  Box,
   Flex,
   Icon,
   Menu,
   MenuButton,
+  MenuDivider,
   MenuItem,
   MenuList,
+  Text,
 } from '@chakra-ui/react';
-import { User } from 'firebase/auth';
+import { signOut, User } from 'firebase/auth';
 import React from 'react';
 import { FaRedditSquare } from 'react-icons/fa';
 import { VscAccount } from 'react-icons/vsc';
 import { IoSparkles } from 'react-icons/io5';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { CgProfile } from 'react-icons/cg';
+import { MdOutlineLogin } from 'react-icons/md';
+import { auth } from '../../../firebase/clientApp';
+import { useSetRecoilState } from 'recoil';
+import { AuthModalState } from '../../../atoms/AuthModalAtom';
 
 type UserMenuProps = {
   // The ? stands for optional
@@ -20,6 +27,7 @@ type UserMenuProps = {
 };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const setAuthModalState = useSetRecoilState(AuthModalState);
   return (
     <Menu>
       <MenuButton
@@ -28,9 +36,9 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
         borderRadius={4}
         _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}
       >
-        {user ? (
+        <Flex align='center'>
           <Flex align='center'>
-            <Flex align='center'>
+            {user ? (
               <>
                 <Icon
                   as={FaRedditSquare}
@@ -38,21 +46,70 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
                   mr={1}
                   color='gray.300'
                 />
+                <Box
+                  display={{ base: 'none', lg: 'flex' }}
+                  flexDirection='column'
+                  fontSize='8pt'
+                  alignItems='flex-start'
+                  mr={8}
+                >
+                  <Text fontWeight={700}>
+                    {user?.displayName || user?.email?.split('@')[0]}
+                  </Text>
+                  <Flex alignItems='center'>
+                    <Icon as={IoSparkles} color='brand.100' mr={1} />
+                    <Text color='gray.400'>1 karma</Text>
+                  </Flex>
+                </Box>
               </>
-              <ChevronDownIcon />
-            </Flex>
+            ) : (
+              <Icon as={VscAccount} fontSize={24} color='gray.400' mr={1} />
+            )}
           </Flex>
-        ) : (
-          <Icon as={VscAccount} fontSize={24} color='gray.400' mr={1} />
-        )}
+          <ChevronDownIcon />
+        </Flex>
       </MenuButton>
       <MenuList>
-        <MenuItem>
-          <Flex align='center'>
-            <Icon as={CgProfile} fontSize={20} mr={2} />
-            Profile
-          </Flex>
-        </MenuItem>
+        {user ? (
+          <>
+            <MenuItem
+              fontSize='10pt'
+              fontWeight={700}
+              _hover={{ bg: 'blue.500', color: 'white' }}
+            >
+              <Flex align='center'>
+                <Icon as={CgProfile} fontSize={20} mr={2} />
+                Profile
+              </Flex>
+            </MenuItem>
+            <MenuDivider />
+            <MenuItem
+              fontSize='10pt'
+              fontWeight={700}
+              _hover={{ bg: 'blue.500', color: 'white' }}
+              onClick={() => signOut(auth)}
+            >
+              <Flex align='center'>
+                <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
+                Log Out
+              </Flex>
+            </MenuItem>
+          </>
+        ) : (
+          <>
+            <MenuItem
+              fontSize='10pt'
+              fontWeight={700}
+              _hover={{ bg: 'blue.500', color: 'white' }}
+              onClick={() => setAuthModalState({ open: true, view: 'login' })}
+            >
+              <Flex align='center'>
+                <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
+                Log In / Sign Up
+              </Flex>
+            </MenuItem>
+          </>
+        )}
       </MenuList>
     </Menu>
   );
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,14 +1,20 @@
 import { Search2Icon } from '@chakra-ui/icons';
 import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { User } from 'firebase/auth';
 import React from 'react';
 
 type SearchInputProps = {
-  // user:
+  user?: User | null;
 };
 
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} mr={2} alignItems='center'>
+    <Flex
+      flexGrow={1}
+      mr={2}
+      alignItems='center'
+      maxWidth={user ? 'auto' : '600px'}
+    >
       <InputGroup>
         <InputLeftElement
           pointerEvents='none'
